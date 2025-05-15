1d3d976f6b31da1d457cb5abb8f29d45f59e5cc4
Hamis Haider <hamis.haider@gmail.com>
2025-05-15 07:55:08 -0600

    working tmux config added

---
+++ dev/config/tmux/clone-git-plugins.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash 
+
+mkdir -p ~/.config/tmux/plugins/catppuccin
+git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+#
+# mkdir -p ~/.config/tmux/plugins/tmux2k
+# git clone https://github.com/2KAbhishek/tmux2k.git ~/.config/tmux/plugins/tmux2k
