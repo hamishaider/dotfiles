b00b7547fe197e22fd61182b63ff51989a460244
hamishaider <hamis.haider@gmail.com>
2025-05-28 10:55:57 -0600

    move scripts from linux branch

---
+++ dev/scripts/archlinx_pacman.sh
@@ -0,0 +1,8 @@
+/////////////////////////////
+///// PACKAGE INSTALL ///////
+/////////////////////////////
+sudo pacman -S ttf-dejavu ttf-liberation noto-fonts ttf-droid -y
+sudo pacman -S keyd -y
+
+
+
